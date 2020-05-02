7e4783d0270226b4972a106a667335cc824c823e
陈文中 <a1260011042@163.com>
2020-05-02 17:35:41 +0800

    fix(新版空调模板):新增调试模式下模拟cordova接口

--- plugin-mould/air-conditioning-new/src/mixins/debug.js
+++ plugin-mould/air-conditioning-new/src/mixins/debug.js
@@ -1,5 +1,6 @@
 import { mapState, mapMutations, mapActions } from 'vuex';
 import updateStatus from './updateStatus'; // 自定义初始化功能，可以修改
+import { Dialog } from 'gree-ui';
 
 const mixin = {
   mixins: [updateStatus],
@@ -11,11 +12,21 @@ const mixin = {
   mounted() {
     this.setCheckObject(this.dataObject);
     this.machineInit();
+    navigator.PluginInterface = navigator.PluginInterface || {};
+    navigator.PluginInterface.showConfirm = (title, content, callback) => {
+      const onConfirm = () => {
+        callback(1);
+      };
+      const onCancel = () => {
+        callback(0);
+      };
+      const onHide = onCancel;
+      Dialog.confirm({ title, content, onConfirm, onCancel, onHide });
+    };
   },
   methods: {
     ...mapMutations('control', {
-      setCheckObject: 'SET_CHECK_OBJECT',
-      setDataObject: 'SET_DATA_OBJECT'
+      setCheckObject: 'SET_CHECK_OBJECT'
     }),
     ...mapActions('machine', {
       machineInit: 'INIT'
