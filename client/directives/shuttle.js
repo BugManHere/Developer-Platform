b1809792bf28b13f1fda82a479034066194f830a
陈文中 <a1260011042@163.com>
2021-01-06 21:30:07 +0800

    feat(平台):更新全局指令方法

---
+++ client/directives/shuttle.js
@@ -0,0 +1,34 @@
+const animationSecond = 0.5;
+
+const transitionSlow = {
+  bind: (el, bingding) => {
+    el.dataset.shuttleTimer = 'false'; // 定时器
+    el.dataset.shuttleDisplay = 'false'; // 是否显示
+    if (!bingding.value) {
+      el.dataset.shuttleDisplay = el.style.display;
+      el.style.display = 'none';
+    }
+  },
+  update: (el, bingding) => {
+    if (bingding.value === bingding.oldValue || el.dataset.shuttleTimer !== 'false') return;
+    if (bingding.value) {
+      if (el.dataset.shuttleDisplay !== 'false') {
+        clearTimeout(el.dataset.shuttleTimer);
+        el.style.display = el.dataset.shuttleDisplay;
+        el.style.animation = `slowShow ${animationSecond}s infinite`;
+        el.style['animation-iteration-count'] = '1';
+      }
+    } else {
+      clearTimeout(el.dataset.shuttleTimer);
+      el.dataset.shuttleTimer = setTimeout(() => {
+        el.dataset.shuttleTimer = 'false';
+        el.dataset.shuttleDisplay = el.style.display;
+        el.style.display = 'none';
+      }, animationSecond * 900);
+      el.style.animation = `slowHide ${animationSecond}s infinite`;
+      el.style['animation-iteration-count'] = '1';
+    }
+  }
+};
+
+export default transitionSlow;
