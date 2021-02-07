56490e3b20a7ce935eec6bb1835b4a819281d4e5
陈文中 <a1260011042@163.com>
2021-02-07 15:48:05 +0800

    feat(音乐空调模板):修改目录名

---
+++ plugin-mould/air-conditioning-music/src/components/card/control/content/fan.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="fan-content" v-unfold="fanAbleSet">
+    <div class="fan-content-header" v-text="'风速'" />
+    <div class="fan-content-main" :class="{ 'set-gray': isGray }">
+      <!-- 圆环 -->
+      <div class="fan-content-main-circle">
+        <div
+          v-for="(fan, index) in fanData"
+          :key="`circle_${index}`"
+          :class="{ select: !isGray && fan.type }"
+          class="circle"
+          @click="fan.func(fan.key, isGray)"
+        />
+        <div class="line" />
+      </div>
+      <!-- 文字 -->
+      <div class="fan-content-main-txt">
+        <div
+          v-for="(fan, index) in fanData"
+          :key="`txt_${index}`"
+          :class="{ select: !isGray && fan.type }"
+          class="txt"
+          v-text="fan.name"
+          @click="fan.func(fan.key, isGray)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters } from 'vuex';
+
+export default {
+  data() {
+    return {
+      fanStatusNameList: [] // 风档的顺序
+    };
+  },
+  mounted() {
+    this.updateStatusNameList();
+  },
+  computed: {
+    ...mapState('control', {
+      Pow: state => state.dataObject.Pow
+    }),
+    ...mapGetters(['fanDefine', 'fanIdentifier', 'fanCurrentStatusName', 'fanAbleSet', 'fanLoop']),
+    fanData() {
+      const result = this.fanStatusNameList.map(fanStatusName => {
+        // status定义
+        const status = this.fanDefine.statusDefine[fanStatusName];
+        // 定义key
+        const key = fanStatusName;
+        // 开关图标
+        const type = this.fanCurrentStatusName === fanStatusName;
+        // 名称
+        const statusNameText = status.name;
+        const stateNameText = this.$language(`fan.${this.fanIdentifier}_${statusNameText}`);
+        // 执行的函数
+        const func = (fanStatusName, disable = false) => {
+          disable || this.$stateMachine.toStatus(this.fanIdentifier, fanStatusName);
+        };
+        return { name: stateNameText, key, func, type };
+      });
+      return result;
+    },
+    isGray() {
+      return !this.Pow;
+    }
+  },
+  watch: {
+    fanLoop() {
+      this.updateStatusNameList();
+    },
+    fanCurrentStatusName() {
+      this.updateStatusNameList();
+    }
+  },
+  methods: {
+    // 给风挡排序
+    updateStatusNameList() {
+      let startStatus = 'default';
+      if (this.fanLoop) {
+        const result = [...this.fanLoop];
+        if (!result.includes(startStatus)) {
+          result.sort((statusNameA, statusNameB) => {
+            return statusNameA[statusNameA.length - 1] - statusNameB[statusNameB.length - 1];
+          });
+          startStatus = result[0];
+        }
+        const length = result.length;
+        let i = 0;
+        while (result[0] !== startStatus && i < length) {
+          result.push(result.shift());
+          i += 1;
+        }
+        this.fanStatusNameList = result;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+$fontSize: 44px;
+.fan-content {
+  position: relative;
+  height: $fanContentHeight;
+  background-color: #fff;
+  font-size: $fontSize;
+  padding: 0 66px;
+  overflow: hidden;
+  &-header {
+    padding: 52px 0px 0 8px;
+    border-top: 1px solid rgba(238, 238, 238, 1);
+    color: rgba(64, 70, 87, 1);
+    font-size: 48px;
+  }
+  &-main {
+    position: relative;
+    &.set-gray {
+      opacity: 0.4;
+    }
+    &-circle {
+      position: relative;
+      margin: 64px 20px 0 20px;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: nowrap;
+      width: auto;
+      .circle {
+        position: relative;
+        height: 40px;
+        width: 40px;
+        border-radius: 100%;
+        background-color: rgba(228, 229, 231, 1);
+        z-index: 2;
+        &.select {
+          background-color: rgba(0, 174, 255, 1);
+        }
+      }
+      .line {
+        position: absolute;
+        left: 0;
+        top: calc(50% - 0.5px);
+        width: 100%;
+        border-bottom: 1px solid rgba(228, 229, 231, 1);
+      }
+    }
+    &-txt {
+      padding-top: 40px;
+      position: relative;
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+      font-size: 42px;
+      color: #b9bbc0;
+      .select {
+        color: rgba(0, 174, 255, 1);
+      }
+    }
+  }
+}
+</style>
---
+++ plugin-mould/air-conditioning-music/src/components/card/grown/skills/search/Result.vue
@@ -0,0 +1,173 @@
+<template>
+  <share v-model="keyword" @search="onSearch" @reload="onReload" :show-error="isLoadFailed">
+    <div class="search-result-panel">
+      <span class="tips" v-show="!isSearching && tips">
+        {{ tips }}
+      </span>
+      <gree-list v-show="!isSearching && data && data.length">
+        <gree-list-item
+          v-for="item in data"
+          :key="item.id"
+          link
+          :title="item.name"
+          :footer="`“${item.illustrate}”`"
+          media-item
+          @click.native="gotoDetail(item)"
+        >
+          <img :src="item.iosIcon" slot="media" class="skill-icon" />
+        </gree-list-item>
+      </gree-list>
+    </div>
+  </share>
+</template>
+
+<script>
+import { List, Item } from 'gree-ui';
+import Share from './Share';
+import { showLoading, hideLoading, voiceACgetSkillSearch } from '@PluginInterface';
+import { mapState } from 'vuex';
+
+export default {
+  components: {
+    [List.name]: List,
+    [Item.name]: Item,
+    share: Share
+  },
+  data() {
+    return {
+      keyword: '',
+      data: [],
+      isSearching: false,
+      tips: '',
+      isLoadFailed: false
+    };
+  },
+  computed: {
+    ...mapState('control', {
+      mac: state => state.mac
+    })
+  },
+  beforeRouteEnter(to, from, next) {
+    console.log(from);
+    if (from.name === 'SkillDetail') {
+      next(vm => {
+        vm.init(from);
+      });
+    } else if (from.name === 'SkillSearchIndex') {
+      next(vm => {
+        vm.init(vm.$route);
+      });
+    } else {
+      next();
+    }
+  },
+  methods: {
+    init(route) {
+      if (route.query && route.query.keyword) {
+        this.keyword = route.query.keyword;
+        this.search(this.keyword);
+      } else {
+        this.search('');
+      }
+    },
+    gotoDetail(item) {
+      this.$router.push(`/SkillDetail/${item.id}?name=${item.name}&icon=${item.iosIcon}&keyword=${this.keyword}`);
+    },
+    onReload() {
+      console.log('onReload');
+      this.search(this.keyword);
+    },
+    onSearch() {
+      this.search(this.keyword);
+    },
+    async search(keyword) {
+      try {
+        this.isLoadFailed = false;
+        this.isSearching = true;
+        this.data = [];
+        this.tips = '';
+        showLoading();
+        let result = await voiceACgetSkillSearch(this.mac, keyword);
+        hideLoading();
+        console.log(result);
+        this.isSearching = false;
+        if (!result) {
+          throw new Error('查询结果为空');
+        }
+
+        if (typeof result === 'string') {
+          result = JSON.parse(result);
+        }
+        if (typeof result.data !== 'undefined') {
+          if (result.data.length > 0) {
+            this.data = result.data;
+          } else {
+            this.tips = '对不起，没有搜到相关技能';
+          }
+        } else {
+          this.tips = '对不起，没有搜到相关技能';
+        }
+      } catch (error) {
+        console.log(error);
+        this.isLoadFailed = true;
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.search-result-panel {
+  height: 100%;
+  overflow-y: auto;
+  .tips {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 45px;
+    z-index: 3;
+    color: rgba($color: #404657, $alpha: 0.5);
+  }
+  /deep/ .list {
+    margin: 0;
+    padding: 0px 36px;
+    ul {
+      &::after,
+      &::before {
+        visibility: hidden;
+      }
+    }
+    .item-content {
+      padding: 42px 0px 0px 42px;
+      .item-media {
+        align-self: flex-start;
+        .skill-icon {
+          width: 107px;
+          height: 107px;
+          padding: 0;
+        }
+      }
+      .item-inner {
+        margin-left: 72px;
+        // border-bottom: 1px solid #f4f4f4;
+        .item-title {
+          font-size: 48px;
+          color: #404657;
+          .item-footer {
+            margin: 24px 0px;
+            font-size: 36px;
+            color: rgba($color: #404657, $alpha: 0.6);
+          }
+        }
+        &::before {
+          background-image: url('../../../../../assets/img/skill/voice_input_search.png');
+          width: 40px;
+          height: 40px;
+          right: 61px;
+        }
+      }
+    }
+  }
+}
+</style>
